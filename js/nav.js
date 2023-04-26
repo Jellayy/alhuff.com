caf4d045ea796df741719fb6ff30c4e46ebacb27
Austin Huffman <austinlynnhuffman@gmail.com>
2023-04-26 11:41:09 -0700

    feat: paginated card redesign

---
+++ js/nav.js
@@ -0,0 +1,37 @@
+// Manages navbuttons in card header
+
+
+// On Load
+$(function() {
+    // Show proper section based on link (default to photography)
+    const url = new URL(window.location)
+    if(url.hash == "#development") {
+        showDevelopment();
+    }
+    else {
+        showPhotography();
+    }
+
+    // Photography nav handler
+    $('#photography-link').click(function() {
+        showPhotography();
+    })
+
+    // Development nav handler
+    $('#development-link').click(function() {
+        showDevelopment();
+    })
+})
+
+
+function showPhotography() {
+    $('#card-content-wrapper').css("transform", "translateX(0)");
+    $('#photography-link').addClass('selected');
+    $('#development-link').removeClass('selected');
+}
+
+function showDevelopment() {
+    $('#card-content-wrapper').css("transform", `translateX(-${$("#photography").width()}px)`);
+    $('#development-link').addClass('selected');
+    $('#photography-link').removeClass('selected');
+}
